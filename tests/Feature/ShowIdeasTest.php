28b527e55d9dd0bafaf970ba5a065e73fa24c0b8
Alexotieno1717 <alexotieno900@gmail.com>
2022-05-18 11:33:56 +0300

    Episode - 10 Testing Idea complete

--- tests/Feature/ShowIdeasTest.php
+++ tests/Feature/ShowIdeasTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\Models\Category;
 use App\Models\Idea;
+use App\Models\Status;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -11,22 +13,37 @@ class ShowIdeasTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function list_of_ideas_show_on_main_page()
+    public function list_of_ideas_shows_on_main_page()
     {
+        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+        $categoryTwo = Category::factory()->create(['name' => 'Category 2']);
+
+        $statusOpen = Status::factory()->create(['name' => 'OpenUnique']);
+        $statusConsidering = Status::factory()->create(['name' => 'ConsideringUnique']);
+
         $ideaOne = Idea::factory()->create([
             'title' => 'My First Idea',
-            'description' => 'My first idea description',
+            'category_id' => $categoryOne->id,
+            'status_id' => $statusOpen->id,
         ]);
 
         $ideaTwo = Idea::factory()->create([
             'title' => 'My Second Idea',
-            'description' => 'My second idea description',
+            'category_id' => $categoryTwo->id,
+            'status_id' => $statusConsidering->id,
         ]);
 
         $response = $this->get(route('idea.index'));
 
         $response->assertSuccessful();
+        $response->assertSee($ideaOne->title);
         $response->assertSee($ideaOne->description);
+        $response->assertSee($categoryOne->name);
+        $response->assertSee('OpenUnique');
+        $response->assertSee($ideaTwo->title);
+        $response->assertSee($ideaTwo->description);
+        $response->assertSee($categoryTwo->name);
+        $response->assertSee('ConsideringUnique');
     }
 
 }
